7d3f7dd58313c6918978535f00d535daa8b814b4
yinanzhu12 <welltempered.ivan@gmail.com>
2017-12-04 14:19:51 -0600

    update

--- reading svm file.R
+++ reading svm file.R
@@ -4,31 +4,7 @@
 
 library(readr)
 library(Matrix)
-
-read_svmlight_class = function(myfile, format='sparseMatrix', num_cols = NULL) {
-  require(Matrix)
-  require(readr)
-  
-  raw_x = read_lines(myfile)
-  x = strsplit(raw_x, ' ', fixed=TRUE)
-  x = lapply(x, function(y) strsplit(y, ':', fixed=TRUE))
-  l = lapply(x, function(y) as.numeric(unlist(y)))
-  label = as.integer(lapply(l, function(x) x[1]))
-  num_rows = length(label)
-  features = lapply(l, function(x) tail(x,-1L))
-  row_length = as.integer(lapply(features, function(x) length(x)/2))
-  features = unlist(features)
-  i = rep.int(seq_len(num_rows), row_length)
-  j = features[seq.int(1, length(features), by = 2)] + 1
-  v = features[seq.int(2, length(features), by = 2)]
-  
-  if(missing(num_cols)) {
-    num_cols = max(j)
-  }
-  m = sparseMatrix(i=i, j=j, x=v, dims=c(num_rows, num_cols))
-  
-  list(labels=label, features=m)
-}
+source('~/GitHub/Sparse-Lasso/gradient descent functions.R')
 
 base_dir = "~/url_svmlight/"
 svm_files = dir(base_dir, pattern = "*.svm")
